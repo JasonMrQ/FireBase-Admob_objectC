f58fb473fe70fadccc33946bf879875c28cff705
Jason <qb@wolf.cc>
2018-03-01 17:28:52 +0800

    !+ Banner Ad Test

--- FireBaseTest/ViewController.m
+++ FireBaseTest/ViewController.m
@@ -12,6 +12,10 @@
 
 @interface ViewController ()
 @property(nonatomic, strong) GADInterstitial*interstitial;
+
+// + banner ad
+@property(nonatomic, strong) GADBannerView *bannerView;
+
 @end
 
 @implementation ViewController
@@ -26,6 +30,16 @@
 //    [self.interstitial loadRequest:request];
     
     self.interstitial =  [self createAndLoadInterstitial];
+    
+    //+ banner ad
+    // In this case, we instantiate the banner with desired ad size.
+    self.bannerView = [[GADBannerView alloc]
+                       initWithAdSize:kGADAdSizeBanner];
+    
+    [self addBannerViewToView:self.bannerView];
+    self.bannerView.adUnitID = @"ca-app-pub-3940256099942544/2934735716";
+    self.bannerView.rootViewController = self;
+    [self.bannerView loadRequest:[GADRequest request]];
 }
 
 - (GADInterstitial *)createAndLoadInterstitial {
@@ -48,6 +62,29 @@
     else{
         NSLog(@"Ad wasn't ready");
     }
+}
+
+
+//+ banner ad
+- (void)addBannerViewToView:(UIView *)bannerView {
+    bannerView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:bannerView];
+    [self.view addConstraints:@[
+                                [NSLayoutConstraint constraintWithItem:bannerView
+                                                             attribute:NSLayoutAttributeBottom
+                                                             relatedBy:NSLayoutRelationEqual
+                                                                toItem:self.bottomLayoutGuide
+                                                             attribute:NSLayoutAttributeTop
+                                                            multiplier:1
+                                                              constant:0],
+                                [NSLayoutConstraint constraintWithItem:bannerView
+                                                             attribute:NSLayoutAttributeCenterX
+                                                             relatedBy:NSLayoutRelationEqual
+                                                                toItem:self.view
+                                                             attribute:NSLayoutAttributeCenterX
+                                                            multiplier:1
+                                                              constant:0]
+                                ]];
 }
 
 
